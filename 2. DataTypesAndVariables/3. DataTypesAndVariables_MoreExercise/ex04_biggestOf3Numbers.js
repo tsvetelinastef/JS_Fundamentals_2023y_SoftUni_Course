16777a198a462aad56dfe07380cd78c6dfd55fe9
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-01-30 14:42:09 +0200

    Add files via upload

---
+++ 2. DataTypesAndVariables/3. DataTypesAndVariables_MoreExercise/ex04_biggestOf3Numbers.js
@@ -0,0 +1,11 @@
+function biggestOf3Numbers(firstNumber, secondNumber, thirdNumber){
+    let max = Math.max(firstNumber, secondNumber, thirdNumber);
+    console.log(max);
+
+}
+
+biggestOf3Numbers(-2, 7, 3); // 7
+biggestOf3Numbers(130, 5, 99); // 130
+biggestOf3Numbers(43, 43.2, 43.1); // 43.2
+biggestOf3Numbers(2, 2, 2 ); // 2
+
---
+++ 2. DataTypesAndVariables/3. DataTypesAndVariables_MoreExercise/ex05_binaryToDecimal.js
@@ -0,0 +1,9 @@
+function binaryToDecimal(binary){
+    let decimal = parseInt(binary, 2);
+    console.log(decimal);
+}
+
+binaryToDecimal(00001001); // 9
+binaryToDecimal(11110000); // 240
+
+
