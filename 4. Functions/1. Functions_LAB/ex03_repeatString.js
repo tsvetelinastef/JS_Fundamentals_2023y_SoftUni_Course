6f08cc69ec5261601717561d2b04265fdfde01ef
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-02-16 10:25:21 +0200

    Update ex03_repeatString.js

--- 4. Functions/1. Functions_LAB/ex03_repeatString.js
+++ 4. Functions/1. Functions_LAB/ex03_repeatString.js
@@ -1,3 +1,27 @@
-function repeatString(){
-    
+function repeatString(str, repeatNum){
+   let result = copyRepeatString(str, repeatNum);
+   return result;
+
+   function copyRepeatString(str, repeatNum){
+        return str.repeat(repeatNum);
+   }
 }
+
+console.log(repeatString('abc', 3));
+console.log(repeatString("String", 2 ));
+
+/*
+------------- Напълно ок  --------------------
+function repeatString(str, repeatString){
+    let result = copyRepeatString(str, repeatNum);
+    return result;
+
+    return str.repeat(repeatNum);
+}
+
+function copyRepeatString(str, repeatNum){
+    return str.repeat(repeatNum);
+}
+
+console.log(repeatString("String", 2));
+*/
