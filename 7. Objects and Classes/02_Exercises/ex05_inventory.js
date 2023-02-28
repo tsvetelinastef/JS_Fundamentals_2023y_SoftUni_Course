3333827154c68b238b26ee8f14ad4f119ef3979a
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-02-28 23:55:28 +0200

    Add files via upload

---
+++ 7. Objects and Classes/02_Exercises/ex05_inventory.js
@@ -0,0 +1,12 @@
+function inventory(data){
+   let res = [];
+
+   for(let line of data){
+        let [name, level, items] = line.split("/");
+        let heroes = {
+            name
+        }
+   }
+} 
+
+inventory();
