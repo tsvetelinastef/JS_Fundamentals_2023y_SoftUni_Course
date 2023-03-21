d37ff4e728b9eb802c6cb8e7889f921532c00f0a
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-03-22 00:12:37 +0200

    Update ex01_furniture.js

--- 31. RegularExpressions/02. RegularExpression_Exrcise/ex01_furniture.js
+++ 31. RegularExpressions/02. RegularExpression_Exrcise/ex01_furniture.js
@@ -1,5 +1,26 @@
-function furniture(data){
+function furniture(data) {
     let pattern = />>(?<name>[A-Z][A-Za-z]+)<<(?<price>[0-9.]+)!(?<quantity>\d+)/;
-    let 
 
+    let totalMoney = 0;
+    console.log("Bought furniture:");
+
+    for (let line of data) {
+        if (line === "Purchase") {
+            break;
+        }
+
+        if (pattern.test(line)) {
+            let product = pattern.exec(line);
+            let price = Number(product.groups.price) * Number(product.groups.quantity);
+
+            console.log(product.groups.name);
+            totalMoney += price;
+        }
+    }
+    console.log(`Total money spend: ${totalMoney.toFixed(2)}`);
 }
+
+furniture(['>>Sofa<<312.23!3',
+'>>TV<<300!5',
+'>Invalid<<!5',
+'Purchase']);
