19e93eb1e0fd7dabbfd3733c8fc2ab492ade9e6f
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-03-23 00:35:26 +0200

    Update ex3_softUniBarlncome.js

--- 31. RegularExpressions/02. RegularExpression_Exrcise/ex03_softUniBarIncome.js
+++ 31. RegularExpressions/02. RegularExpression_Exrcise/ex03_softUniBarIncome.js
@@ -1,6 +1,6 @@
 function softUniBarIncome(data){
     //let pattern = /%(?<name>[A-Z][a-z]+)%[^|$%.]*<(?<product>\w+)>[^|$%.]*\|(?<count>\d+)\|[^|$%.0-9]*([0-9]+.?\d*)\$/;
-    let pattern = /%(?<name>[A-Z][a-z]+)%[^|$%.]*<(?<product>\w+)>[^|$%.]*\|(?<count>\d+)\|[^|$%.0-9]*([0-9]+.?\d*)\$/gm;
+    let pattern = /%(?<name>[A-Z][a-z]+)%[^|$%.]*<(?<product>\w+)>[^|$%.]*\|(?<count>\d+)\|[^|$%.0-9]*(?<>[0-9]+.?\d*)\$/;
     let total = 0;
 
     for(let line of data){
@@ -9,8 +9,15 @@ function softUniBarIncome(data){
             break;
         }
 
-        
+        if(pattern.test(line)){
+            match = line.match(pattern);
+            let price = Number(match.groups.count) * Number(match.groups.price);
+            total += price;
+            console.log(`${match.groups.name}: ${match.groups.product} - ${price.toFixed(2)}`);
+        }
     }
+
+    console.log(`Total income: ${total.toFixed(2)}`);
 }
 
 softUniBarIncome(['%George%<Croissant>|2|10.3$',
@@ -18,8 +25,10 @@ softUniBarIncome(['%George%<Croissant>|2|10.3$',
 '%Maria%<Cola>|1|2.4$',
 'end of shift']);
 
+/*
 softUniBarIncome(['%InvalidName%<Croissant>|2|10.3$',
 '%Peter%<Gum>1.3$',
 '%Maria%<Cola>|1|2.4',
 '%Valid%<Valid>valid|10|valid20$',
 'end of shift']);
+*/
