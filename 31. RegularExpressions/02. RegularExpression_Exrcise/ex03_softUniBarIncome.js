ff318c3133ae775ba5242a57d58bb4b0b7705b7f
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-03-22 23:10:30 +0200

    Create ex03_softUniBarlncome.js

---
+++ 31. RegularExpressions/02. RegularExpression_Exrcise/ex03_softUniBarIncome.js
@@ -0,0 +1,25 @@
+function softUniBarIncome(data){
+    //let pattern = /%(?<name>[A-Z][a-z]+)%[^|$%.]*<(?<product>\w+)>[^|$%.]*\|(?<count>\d+)\|[^|$%.0-9]*([0-9]+.?\d*)\$/;
+    let pattern = /%(?<name>[A-Z][a-z]+)%[^|$%.]*<(?<product>\w+)>[^|$%.]*\|(?<count>\d+)\|[^|$%.0-9]*([0-9]+.?\d*)\$/gm;
+    let total = 0;
+
+    for(let line of data){
+        let match = [];
+        if(line === "end of shift"){
+            break;
+        }
+
+        
+    }
+}
+
+softUniBarIncome(['%George%<Croissant>|2|10.3$',
+'%Peter%<Gum>|1|1.3$',
+'%Maria%<Cola>|1|2.4$',
+'end of shift']);
+
+softUniBarIncome(['%InvalidName%<Croissant>|2|10.3$',
+'%Peter%<Gum>1.3$',
+'%Maria%<Cola>|1|2.4',
+'%Valid%<Valid>valid|10|valid20$',
+'end of shift']);
