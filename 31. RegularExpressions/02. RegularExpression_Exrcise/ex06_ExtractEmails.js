80f4f51f368ac7b97f2616abf21377efe30bb049
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-03-29 22:39:22 +0300

    Update ex06_ExtractEmails.js

--- 31. RegularExpressions/02. RegularExpression_Exrcise/ex06_ExtractEmails.js
+++ 31. RegularExpressions/02. RegularExpression_Exrcise/ex06_ExtractEmails.js
@@ -1,6 +1,12 @@
-function extractEmails(){
-    let pattern = /(?<=\s|^)()@[a-Z]+\-?[a-z] + (\.[a-z]+) + /g;
+function extractEmails(text){
+    let pattern = /(?<=\s|^)([A-Za-z0-9]+[A-Za-z0-9.\-_]*)@[a-z]+\-?[a-z]+(\.[a-z]+)+/gm;
+    let emails = text.match(pattern); 
 
-    
+    for(let email of emails){
+        console.log(email);
+    }
 
 }
+
+extractEmails("Please contact us at: support@github.com.");
+//extractEmails()
