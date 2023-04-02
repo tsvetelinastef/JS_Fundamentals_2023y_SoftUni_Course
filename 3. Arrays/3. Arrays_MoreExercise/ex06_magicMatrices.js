d42fb27cef666b32aa6838904f74826109b06b23
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-04-02 09:38:05 +0300

    Create ex06_magicMatrics.js

---
+++ 3. Arrays/3. Arrays_MoreExercise/ex06_magicMatrices.js
@@ -0,0 +1,13 @@
+function magicMatrices(){
+    
+}
+
+magicMatrices([[4, 5, 6],
+    [6, 5, 4],
+    [5, 5, 5]]); // true
+magicMatrices([[11, 32, 45],
+    [21, 0, 1],
+    [21, 1, 1]]); // false
+magicMatrices([[1, 0, 0],
+    [0, 0, 1],
+    [0, 1, 0]]); // true
